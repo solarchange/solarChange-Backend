d1c6ad71416afc3f517692da98e1368b82c27928
urihamster <uri.h.y.k@gmail.com>
2016-06-27 19:44:11 +0200

    asdf
    sadf

--- assets/js/dependencies/admin_login.js
+++ assets/js/dependencies/admin_login.js
@@ -3,6 +3,7 @@
 $(document).ready(function(){
 
 	$('#login-submit').click(function(){
+		console.log('jaooooooooooooo')
 		var da_cookie = 'email='+$('#email-login').val()+'; pass='+$('#pass-login').val()+';';
 		document.cookie = da_cookie;
 	});
