5ab174fa73c9485508e26ef56766fbb25368e321
urihamster <uri.h.y.k@gmail.com>
2016-06-27 18:02:19 +0200

    adsf
    adsf

--- api/policies/isAuthenticated.js
+++ api/policies/isAuthenticated.js
@@ -3,8 +3,8 @@ var passport = require('passport');
 
 module.exports = function(req, res, next) {
 
-
    if (req.isAuthenticated()) {
+   	console.log('hoooooray fuck fuck')
         return next();
     }
     else{
