f5173af9f9e87f7f761bfd27682bda291b3a4891
urihamster <uri.h.y.k@gmail.com>
2016-06-27 19:00:50 +0200

    lk
    lj

--- api/policies/isAuthenticated.js
+++ api/policies/isAuthenticated.js
@@ -3,7 +3,7 @@ var passport = require('passport');
 
 module.exports = function(req, res, next) {
 
- if req.isSocket
+ if (req.isSocket)
  {
         req = _.extend req, _.pick(require('http').IncomingMessage.prototype, 'login', 'logIn', 'logout', 'logOut', 'isAuthenticated', 'isUnauthenticated')
     middleware = passport.authenticate('bearer', { session: false })
