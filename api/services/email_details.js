099d63e590f2c1d610ec89961a094e54d6987df9
urihamster <uri.h.y.k@gmail.com>
2016-06-29 12:35:54 +0200

    sadf
    sdf

--- api/services/email_details.js
+++ api/services/email_details.js
@@ -4,7 +4,7 @@ module.exports = {
 
 	//base_url:'http://staging.solarchange.co/#/',
 
-	base_url:'http://www.solarchange.co/#/'
+	base_url:'http://www.solarchange.co/#/',
 
 	base_image_url: 'staging.solarchange.co',	
 
