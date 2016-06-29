44e6616cb277fbe5051a90af8e1c3bcfc273925c
urihamster <uri.h.y.k@gmail.com>
2016-06-29 12:35:02 +0200

    af
    sad

--- api/services/email_details.js
+++ api/services/email_details.js
@@ -2,7 +2,9 @@
 
 module.exports = {
 
-	base_url:'http://staging.solarchange.co/#/',
+	//base_url:'http://staging.solarchange.co/#/',
+
+	base_url:'http://www.solarchange.co/#/'
 
 	base_image_url: 'staging.solarchange.co',	
 
